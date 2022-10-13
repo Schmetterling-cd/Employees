0dfb0e7f246dd978c79b28bbb5889a0743844f91
Schmetterling-cd <103847870+Schmetterling-cd@users.noreply.github.com>
2022-10-14 02:24:22 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 include_once '/var/www/vlad/Employees/autoload.php';
-    define("DB_URL", "localhost");
-    define("DB_NAME", "Company");
-    define("DB_USER", "root");
-    define("DB_PASSWORD", "5240102H000PB5");
+    define("DB_URL", "YOUR_LINK");
+    define("DB_NAME", "YOUR_DATABASE");
+    define("DB_USER", "YOUR_USERNAME");
+    define("DB_PASSWORD", "YOUR_PASSWORD");
 ?>
